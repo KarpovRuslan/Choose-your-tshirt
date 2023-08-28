a42de708b2399bb3514821c7b329002c0e786b28
Ruslan Karpov <rkarpov88@gmail.com>
2023-08-28 14:26:56 +0300

    first commit

---
+++ client/src/canvas/BackDrop.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const BackDrop = () => {
+  return <div>BackDrop</div>;
+};
+
+export default BackDrop;
---
+++ client/src/canvas/index.jsx
@@ -0,0 +1,8 @@
+import { Canvas } from "@react-three/fiber";
+import { Environment, Center } from "@react-three/drei";
+
+const CanvasModel = () => {
+  return <div>CanvasModel</div>;
+};
+
+export default CanvasModel;
