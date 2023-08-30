333133d153a23e107b285ccc338c26f2ed3d26d9
Ruslan Karpov <rkarpov88@gmail.com>
2023-08-30 13:50:39 +0300

    edit customizer

--- client/src/components/Tab.jsx
+++ client/src/components/Tab.jsx
@@ -17,7 +17,13 @@ const Tab = ({ tab, isFilterTab, isActiveTab, handleClick }) => {
       }`}
       onClick={handleClick}
     >
-      <img src={tab.icon} alt="" />
+      <img
+        src={tab.icon}
+        alt={tab.name}
+        className={`${
+          isFilterTab ? "w-2/3 h-2/3" : "w-11/12 h-11/12 object-contain"
+        }`}
+      />
     </div>
   );
 };
