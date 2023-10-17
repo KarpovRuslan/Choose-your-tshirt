0a06c9ede4203a4e244221651fcccf1afd2b51ac
Ruslan Karpov <103334343+KarpovRuslan@users.noreply.github.com>
2023-10-17 17:23:07 +0300

    Update Customizer.jsx
    fix backend link

--- client/src/pages/Customizer.jsx
+++ client/src/pages/Customizer.jsx
@@ -50,7 +50,7 @@ const Customizer = () => {
     try {
       setGeneratingImg(true);
       console.log(prompt);
-      const response = await fetch("http://localhost:5050/api/v1/dalle", {
+      const response = await fetch("https://project-tshirt-design.onrender.com/api/v1/dalle", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
